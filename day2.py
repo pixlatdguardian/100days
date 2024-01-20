ca4dcc454a489bbb95db92e1df836a62194b51b4
Trevor Jeffs <trevjeffs@gmail.com>
2024-01-19 20:05:06 -0800

    Cleaned up the code

--- day2.py
+++ day2.py
@@ -1,20 +1,21 @@
 #If the bill was $150.00, split between 5 people, with 12% tip. 
-
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
-
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
-
+#Round the result to 2 decimal places.
 print("Welcome to the tip calculator!")
-bill = input("What was the total bill? $")
-tip = input("How much tip would you like to give? 10, 12, or 15? ")
-people = input("How many people to split the bill? ")
-bill_float = float(bill)
-tip_int = int(tip)
-people_int = int(people)
-total_bill = bill_float * (1+(tip_int/100))
-bill_per_person = total_bill / people_int
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
 final_amount = round(bill_per_person, 2)
-print(f"Each person will owe ${final_amount}.")
+
+
+# FAQ: How to round to 2 decimal places?
+
+# Find the answer in the Q&A here: https://www.udemy.com/course/100-days-of-code/learn/lecture/17965132#questions/13315048
+
+
+print(f"Each person should pay: ${final_amount}")
