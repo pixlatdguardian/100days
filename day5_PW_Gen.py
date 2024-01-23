02b172dab58d517782f0b9a775e94aaa34bc3e49
Trevor Jeffs <trevjeffs@gmail.com>
2024-01-22 20:42:33 -0800

    Cleaned up code

--- day5_PW_Gen.py
+++ day5_PW_Gen.py
@@ -6,8 +6,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
 # Add letters, numbers, and symbols to the list
 password_list = []
