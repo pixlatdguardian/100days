54e8090f63b1220a6b2e5ddc515001847a660d3f
Trevor Jeffs <trevjeffs@gmail.com>
2024-02-10 14:41:57 -0800

    Added a high score functionality to the snake game, using file management.

--- day20_snake/scoreboard.py
+++ day20_snake/scoreboard.py
@@ -5,17 +5,23 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("high_score.txt", mode="r") as file:
+            scores = file.read()
+            self.high_score = scores
         self.color("white")
         self.penup()  # Make sure to lift the pen up before moving
         self.goto(0, 270)
         self.hideturtle()  # Hide the turtle/arrow representation
         self.update_scoreboard()  # Call a method to update the scoreboard text
 
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=('Arial', 16, 'normal'))
+    def reset_game(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+            with open("high_score.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()  # Clear the previous score before writing the new score
-        self.write(f"Score: {self.score}", align="center", font=('Arial', 16, 'normal'))
+        self.write(f"Score: {self.score} High Score {self.high_score}", align="center", font=('Arial', 16, 'normal'))
